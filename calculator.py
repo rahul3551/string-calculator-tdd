f24fa22a830e91490e2b3eb5f5ee9571f2763ef6
Rahul <rahuliitdhn111@gmail.com>
2025-06-15 16:38:45 +0530

    test_custom_delimiter_semicolon passed

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
 def add(numbers):
     if numbers == "":
         return 0  
-    if "," in numbers or "\n" in numbers:
-        numbers = numbers.replace("\n", ",")
-        return sum(map(int, numbers.split(",")))
-    return int(numbers)
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[3:]
+        numbers = numbers.replace(delimiter, ',')
+       
+    numbers = numbers.replace("\n", ",")
+    return sum([int(i) for i in numbers.split(',') if i])
