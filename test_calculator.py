8282e21371024499a64ca1ce3d7e0ebe68868f7f
Rahul <rahuliitdhn111@gmail.com>
2025-06-15 16:12:00 +0530

    test_new_line_separated_numbers failed

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,6 @@ def test_single_number_returns_itself():
 
 def test_two_numbers_comma_separated():
     assert add("1,2") == 3
+
+def test_new_line_separated_numbers():
+    assert add("1\n2,3") == 6
