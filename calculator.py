2ecba1e7890deb25d7ba7b9f05d74c0aad0ccd6b
Rahul <rahuliitdhn111@gmail.com>
2025-06-15 16:14:09 +0530

    test_new_line_separated_numbers passed too

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 def add(numbers):
     if numbers == "":
         return 0  
-    if "," in numbers:
+    if "," in numbers or "\n" in numbers:
+        numbers = numbers.replace("\n", ",")
+        print(numbers)
         return sum(map(int, numbers.split(",")))
     return int(numbers)
+
+print(add("1\n2,3"))
